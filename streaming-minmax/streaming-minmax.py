6f9b6b1e1b2d297136ee9759caea2318f73460b8
sandeep <45798709+sandeep1404@users.noreply.github.com>
2026-03-17 23:51:40 +0900

    Add solution for streaming-minmax

---
+++ streaming-minmax/streaming-minmax.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def streaming_minmax_init(D):
+    """
+    Initialize state dict with min, max arrays of shape (D,).
+    """
+    # Write code here
+
+    return {
+        "min": np.full(D, np.inf),   # shape (D,), start at +inf
+        "max": np.full(D, -np.inf),  # shape (D,), start at -inf
+    }
+
+
+def streaming_minmax_update(state, X_batch, eps=1e-8):
+    """
+    Update state's min/max with X_batch, return normalized batch.
+    """
+    # Write code here
+    X_batch = np.array(X_batch)
+    state["min"] = np.minimum(state["min"], X_batch.min(axis=0))
+    state["max"] = np.maximum(state["max"], X_batch.max(axis=0))
+
+    X_norm = (X_batch - state["min"]) / (state["max"] - state["min"] + eps)
+
+    return X_norm
+
+
+    
