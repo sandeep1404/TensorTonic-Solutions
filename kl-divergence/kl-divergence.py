7ca7a32abb9835ec3fae178313f5ee84c8de0c98
sandeep <45798709+sandeep1404@users.noreply.github.com>
2026-03-20 16:54:57 +0900

    Add solution for kl-divergence

---
+++ kl-divergence/kl-divergence.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def kl_divergence(p, q, eps=1e-12):
+    """
+    Compute KL Divergence D_KL(P || Q).
+    """
+    # Write code here
+
+
+    p = np.array(p)
+    q = np.array(q)
+
+    out = np.sum(p*np.log(np.divide(p,q+eps)))
+
+    return out
